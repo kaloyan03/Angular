54f153c1e58c4b7ac880bcdb53c0be09149203c8
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-06-23 16:58:50 +0300

    created two components - one manually, one with CLI

--- udemy-first-assignment/src/app/app.module.ts
+++ udemy-first-assignment/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule

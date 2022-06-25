e69421d1fddd18b5119f4bf84858e29e05aa6bfb
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-06-25 23:52:35 +0300

    assignment completed successful

--- udemy-directives-assignment/src/app/app.component.ts
+++ udemy-directives-assignment/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'udemy-directives-assignment';
+
+  isToggled: boolean = false;
+  buttonClickLogs: number[] = [];
+  buttonClicksCounter: number = 0;
+
+  onClickToggleParagraph() {
+    this.buttonClicksCounter++;
+
+    if (this.isToggled) {
+      this.isToggled = false;
+    } else {
+      this.isToggled = true;
+    }
+
+    this.buttonClickLogs.push(this.buttonClicksCounter);
+
+    
+  }
 }

23f911d9f47fa963c28f9e22b66eb144d4ee5382
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-06-26 11:58:46 +0300

    components used, added the navigation bar and styled using bootstrap

--- udemy-course-project-recipies/src/app/app.module.ts
+++ udemy-course-project-recipies/src/app/app.module.ts
@@ -5,24 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipiesComponent } from './recipies/recipies.component';
 import { RecipiesListComponent } from './recipies/recipies-list/recipies-list.component';
-import { RecipieItemComponent } from './recipies/recipies-list/recipie-item/recipie-item.component';
-import { RecipieDetailsComponent } from './recipies/recipie-details/recipie-details.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeDetailsComponent } from './recipies/recipe-details/recipe-details.component';
 import { RecipeItemComponent } from './recipies/recipies-list/recipe-item/recipe-item.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipiesComponent,
     RecipiesListComponent,
-    RecipieItemComponent,
-    RecipieDetailsComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     RecipeDetailsComponent,
-    RecipeItemComponent
+    RecipeItemComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,

4aed468fccd7f6d0dd02a3a556896bb9e82b73d1
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-06-26 11:03:13 +0300

    components created

--- udemy-course-project-recipies/src/app/app.module.ts
+++ udemy-course-project-recipies/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipiesListComponent } from './recipies/recipies-list/recipies-list.component';
+import { RecipieItemComponent } from './recipies/recipies-list/recipie-item/recipie-item.component';
+import { RecipieDetailsComponent } from './recipies/recipie-details/recipie-details.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeDetailsComponent } from './recipies/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './recipies/recipies-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipiesComponent,
+    RecipiesListComponent,
+    RecipieItemComponent,
+    RecipieDetailsComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,

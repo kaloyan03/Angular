aeccbf1f38d3c3ca9fbae780fde95d9e8170929c
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-06-26 13:56:00 +0300

    recipe model created; implementation of listing recipies added;

--- udemy-course-project-recipies/src/app/recipies/recipies-list/recipies-list.component.ts
+++ udemy-course-project-recipies/src/app/recipies/recipies-list/recipies-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipies-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipies-list.component.css']
 })
 export class RecipiesListComponent implements OnInit {
+  recipies: Recipe[] = [
+    new Recipe('Test', 'Just for testing', 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Vegetable-Frittata_EXPS_GBHRBZ17_2209_D07_20_2b.jpg')
+  ];
 
   constructor() { }
 

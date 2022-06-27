90eb186e52509753051ffc43792cd0cb0bf5ec9c
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-06-27 15:04:50 +0300

    custom event binding

--- udemy-components-data-binding-deep-dive/src/app/app.component.ts
+++ udemy-components-data-binding-deep-dive/src/app/app.component.ts
@@ -6,8 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements: any[] = [{type: 'server', name: 'test', content: 'test'}, {type: 'server', name: 'test', content: 'test'}];
-  newServerName: string = '';
-  newServerContent: string = '';
+  serverElements: any[] = [];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 
 }

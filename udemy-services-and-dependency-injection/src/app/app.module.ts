73d5bed28cbd1a7548658b706215ef249eb249c2
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-07-01 12:34:05 +0300

    LoggingService created and it's goal is to log the status of the accounts

--- udemy-services-and-dependency-injection/src/app/app.module.ts
+++ udemy-services-and-dependency-injection/src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    NewAccountComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

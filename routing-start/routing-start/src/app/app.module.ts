875bedd044179a423ce450e62979a7a21814ce71
Kaloyan Tsvetkov <kaloqnbg076@gmail.com>
2022-07-03 15:50:38 +0300

    not found page added

--- routing-start/routing-start/src/app/app.module.ts
+++ routing-start/routing-start/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -22,6 +23,7 @@ const routes: Routes = [
   {path: 'users', component: UsersComponent, children: [
     {path: ':id/:name', component: UserComponent}
   ]},
+  {path: '**', component: PageNotFoundComponent}
 ]
 
 @NgModule({
